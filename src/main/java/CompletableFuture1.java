282c2617b1b0668b7e257dbc6be9405d3be1c60f
keytsai <keytsai513@gmail.com>
2021-04-12 17:32:21 +0800

    CompletableFuture update

--- src/main/java/CompletableFuture1.java
+++ src/main/java/CompletableFuture1.java
@@ -1,19 +1,21 @@
+import java.io.File;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class CompletableFuture1 {
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 
+		ReadCSV4 read1 = new ReadCSV4();
+		ReadCSV4 read2 = new ReadCSV4();
+
 		CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-			try {
-				Thread.sleep(1000);
-				System.out.println("hello");
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			File currentDir = new File("/Users/ckts/Coding/newBilling3/amazon-billing/CUR/CUR1");
+			read1.displayDirectoryFiles(currentDir);
 		});
 
 		future.get();
-		System.out.println("world");
+		File currentDir = new File("/Users/ckts/Coding/newBilling3/amazon-billing/CUR/CUR2");
+		read2.displayDirectoryFiles(currentDir);
 	}
 }
