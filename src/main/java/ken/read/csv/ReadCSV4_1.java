b113de932d929b57475032c476da0387ea726bee
keytsai <keytsai513@gmail.com>
2021-04-16 10:48:53 +0800

    update CompletableFuture and practice feedback
    本次練習難度：8
    
    覺得這次作業真的很有挑戰性
    主要在於
    對java 8 新增語法及API不是很熟悉，如lambda表達式及CompletableFuture
    還有對於多執行序也是頗生疏，例如這次作業會卡在怎麼分出task並放入list中再跑多執行緒等
    感謝Eric給我們練習作業及指導，寫作業的過程真的獲益良多

--- src/main/java/ken/read/csv/ReadCSV4_1.java
+++ src/main/java/ken/read/csv/ReadCSV4_1.java
@@ -8,37 +8,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.ZipInputStream;
-
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ReadCSV4_1 {
 
-	public static void main(String[] args) throws IOException {
-		
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+
 		long startTime = System.currentTimeMillis();
 		System.out.println("開始執行");
-		
+
 		File currentDir = new File("/Users/ckts/Coding/newBilling3/amazon-billing/CUR/");
 		String writeToJsonFilePath = "/Users/ckts/Coding/readTest/ken_feb.json";
 
 		ReadCSV4_1 writeToJsonFile = new ReadCSV4_1();
+		CompletableFuture<Void> task = CompletableFuture.runAsync(() -> {
+
+			try {
+				Map<String, AtomicInteger> map = new HashMap<>();
+				map = writeToJsonFile.displayDirectoryFiles(currentDir);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+		task.get();
 		writeToJsonFile.writeJson(currentDir, writeToJsonFilePath);
 
 		long endTime = System.currentTimeMillis();
-		long totalSeconds = TimeUnit.MILLISECONDS.toSeconds(endTime-startTime);
-        System.out.println("完成，執行時間："+totalSeconds +"秒");
+		long totalSeconds = TimeUnit.MILLISECONDS.toSeconds(endTime - startTime);
+		System.out.println("完成，執行時間：" + totalSeconds + "秒");
+
 	}
 
-	Map<String, Integer> map = new HashMap<>();
+	Map<String, AtomicInteger> map = new HashMap<>();
 	ObjectMapper objectMapper = new ObjectMapper();
 
 	public void writeJson(File currentDir, String writeToJsonFilePath) throws IOException {
-		displayDirectoryFiles(currentDir);
-//		System.out.println(map);
 
 		try {
 			objectMapper.writeValue(new File(writeToJsonFilePath), map);
@@ -49,10 +62,9 @@ public class ReadCSV4_1 {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	public void displayDirectoryFiles(File dir) throws IOException {
+	public Map<String, AtomicInteger> displayDirectoryFiles(File dir) throws IOException {
 		try {
 			File[] files = dir.listFiles();
 			for (File file : files) {
@@ -63,7 +75,7 @@ public class ReadCSV4_1 {
 					ZipInputStream zi = new ZipInputStream(new BufferedInputStream(fi));
 
 					while ((zi.getNextEntry()) != null) {
-						readCSV(zi);
+						map = readCSV(zi);
 					}
 
 					zi.closeEntry();
@@ -74,11 +86,11 @@ public class ReadCSV4_1 {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return map;
 	}
 
-	public void readCSV(ZipInputStream zi) throws IOException {
+	public Map<String, AtomicInteger> readCSV(ZipInputStream zi) throws IOException {
 
-		int countLine = 0;
 		String line = "";
 		String splitBy = ",";
 
@@ -87,14 +99,16 @@ public class ReadCSV4_1 {
 			while ((line = br.readLine()) != null) {
 
 				String[] cells = line.split(splitBy);
-				countLine = map.containsKey(cells[8]) ? map.get(cells[8]) : 0;
-				map.put(cells[8], countLine + 1);
+				AtomicInteger countLine = new AtomicInteger(0);
+				countLine = map.containsKey(cells[8]) ? map.get(cells[8]) : countLine;
+				map.put(cells[8], countLine);
+				map.get(cells[8]).getAndIncrement();
 			}
 			map.remove("lineItem/UsageAccountId");
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return map;
 	}
-
 }
